0d04653782e77605d982de4f0e19436c9ce15a9f
AangSari <dalamkontak@gmail.com>
2025-11-16 02:25:43 +0300

    Create addons.js

---
+++ src/addons.js
@@ -0,0 +1,10 @@
+{
+  "title": "DKon Authentication",
+  "version": "1.0.0",
+  "description": "Allows users to authenticate using DKon API.",
+  "license": "MIT",
+  "namespace": "YourNamespace\\DKonAuth",
+  "routes": {
+    "dkonAuth": "YourNamespace\\DKonAuth\\Controller\\Login"
+  }
+}
